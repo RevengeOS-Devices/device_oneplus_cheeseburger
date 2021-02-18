7acac4f95b09dc9e95d7f4bb44fd0da5772a0a33
karthik990 <knag88@gmail.com>
2021-02-18 17:39:53 +0000

    cheeseburger: POSP bringup

--- potato_cheeseburger.mk
+++ potato_cheeseburger.mk
@@ -24,10 +24,10 @@ $(call inherit-product, device/oneplus/cheeseburger/device.mk)
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# Inherit some common DotOS stuff.
-$(call inherit-product, vendor/dot/config/common.mk)
+# Inherit from Potato vendor
+$(call inherit-product, vendor/potato/config/common_full_phone.mk)
 
-PRODUCT_NAME := dot_cheeseburger
+PRODUCT_NAME := potato_cheeseburger
 PRODUCT_DEVICE := cheeseburger
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
